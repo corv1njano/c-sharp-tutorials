fb9bdb3d758620bb8065086db03fad28c745e801
corv1njano <56222588+corv1njano@users.noreply.github.com>
2025-04-04 11:17:21 +0200

    Add files via upload

---
+++ CustomTitlebarProject/CustomTitlebar/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTitlebar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WindowState PrevWindowState {  get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+
+            BorderThickness = WindowState switch
+            {
+                WindowState.Maximized => WindowHelper.InflateBorder(SystemParameters.WindowResizeBorderThickness),
+                WindowState.Normal or WindowState.Minimized when PrevWindowState == WindowState.Maximized => WindowHelper.DeflateBorder(SystemParameters.WindowResizeBorderThickness),
+                _ => BorderThickness
+            };
+
+            PrevWindowState = WindowState;
+        }
+    }
+}
---
+++ CustomTitlebarProject/CustomTitlebar/WindowHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CustomTitlebar
+{
+    public class WindowHelper
+    {
+        public static Thickness InflateBorder(Thickness thickness)
+        {
+            double extra = 4.0d;
+            double left = Application.Current.MainWindow.BorderThickness.Left + thickness.Left + extra;
+            double top = Application.Current.MainWindow.BorderThickness.Top + thickness.Top + extra;
+            double right = Application.Current.MainWindow.BorderThickness.Right + thickness.Right + extra;
+            double bottom = Application.Current.MainWindow.BorderThickness.Bottom + thickness.Bottom + extra;
+            return new Thickness(left, top, right, bottom);
+        }
+
+        public static Thickness DeflateBorder(Thickness thickness)
+        {
+            double extra = 4.0d;
+            double left = Application.Current.MainWindow.BorderThickness.Left - thickness.Left - extra;
+            double top = Application.Current.MainWindow.BorderThickness.Top - thickness.Top - extra;
+            double right = Application.Current.MainWindow.BorderThickness.Right - thickness.Right - extra;
+            double bottom = Application.Current.MainWindow.BorderThickness.Bottom - thickness.Bottom - extra;
+            return new Thickness(left, top, right, bottom);
+        }
+    }
+}
